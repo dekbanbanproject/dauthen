b96d18d2a8b6d7985e7e514eab1bba27ea7724f3
dekbanbanproject <dekbanbanproject@gmail.com>
2023-12-14 18:55:12 +0700

    q

--- resources/views/authen_main.blade.php
+++ resources/views/authen_main.blade.php
@@ -7,7 +7,7 @@
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>PK-AUTHEN</title>
+    <title>D-AUTHEN</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
